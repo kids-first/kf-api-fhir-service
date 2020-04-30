8f2c0ab046dc8538eb429c0f000327197ac0c88b
Natasha Singh <dukedesi22@gmail.com>
2020-04-30 17:55:37 -0400

    :recycle: Simplify docker img build - source from DH only

--- scripts/run_local_server.sh
+++ scripts/run_local_server.sh
@@ -6,46 +6,8 @@ set -eo pipefail
 
 echo "✔ Begin deploying ..."
 
-DOCKER_TARBALL=${1:-"smilecdr-2020.05.PRE-14-docker.tar.gz"}
-DOCKER_HUB_USERNAME=${DOCKER_HUB_USERNAME}
-DOCKER_HUB_PW=${DOCKER_HUB_PW}
-DOCKER_REPO="kidsfirstdrc/smilecdr"
-DOCKER_IMAGE_TAG=${DOCKER_TARBALL#"smilecdr-"}
-DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG%"-docker.tar.gz"}
-DOCKER_IMAGE=${2:-"$DOCKER_REPO:$DOCKER_IMAGE_TAG"}
-
-# Set env file
-if [[ ! -f '.env' ]]; then
-    cp 'server/settings/dev.env' '.env'
-fi
-
-# Try using a local Smile CDR image if it exists
-if [ "$(docker images -q $DOCKER_IMAGE 2> /dev/null)" != "" ]
-then
-    echo "Using docker image $DOCKER_IMAGE"
-else
-    # Try pulling the image from Docker Hub if it exists
-    echo "Smile CDR docker image $DOCKER_IMAGE not found, try pulling from $DOCKER_REPO ..."
-    if [[ -z $DOCKER_HUB_USERNAME ]] || [[ -z $DOCKER_HUB_PW ]]
-    then
-        source .env
-    fi
-    echo "Logging into Docker Hub ..."
-    echo "$DOCKER_HUB_PW" | docker login -u "$DOCKER_HUB_USERNAME" --password-stdin
-    docker pull $DOCKER_IMAGE
-
-    # Try creating the image from a local tarball
-    if [[ -f $DOCKER_TARBALL ]]; then
-        echo "Loading docker image from tarball: $DOCKER_TARBALL"
-        docker image load --input=$DOCKER_TARBALL
-        docker tag smilecdr:latest $DOCKER_IMAGE
-    fi
-fi
-# Could not fetch or find image - fail
-if [ "$(docker images -q $DOCKER_IMAGE 2> /dev/null)" == "" ]; then
-    echo "Aborting! Could not find or fetch the Smile CDR docker image."
-    exit 1
-fi
+# Build service container
+source ./scripts/build.sh
 
 # Destroy existing docker containers
 docker-compose down
