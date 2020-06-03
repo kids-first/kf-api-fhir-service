c553c446b51cadc210a01463fe6076672dedca29
Natasha Singh <dukedesi22@gmail.com>
2020-06-03 17:10:27 -0400

    :truck: Rename DB env vars to match ecs-serv-typ-1 generated ones

--- scripts/config.py
+++ scripts/config.py
@@ -7,5 +7,5 @@ ENV_FILE = os.path.join(ROOT_DIR, 'smilecdr', '.env')
 load_dotenv(dotenv_path=ENV_FILE)
 
 BASE_URL = os.environ.get('FHIR_BASE_URL')
-ADMIN_USER = os.getenv('DB_CDR_USERID')
-ADMIN_PW = os.getenv('DB_CDR_PASSWORD')
+ADMIN_USER = os.getenv('DB_USERNAME')
+ADMIN_PW = os.getenv('DB_PASSWORD')
