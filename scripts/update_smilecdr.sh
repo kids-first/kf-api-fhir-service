0a7218449556f5c81e77e549883cea148b7b371b
liberaliscomputing <liberalis.computing@gmail.com>
2021-05-24 17:56:17 -0400

    :sparkles: handle aws cli v2

--- scripts/update_smilecdr.sh
+++ scripts/update_smilecdr.sh
@@ -7,11 +7,11 @@
 # DOCKER_HUB_USERNAME DOCKER_HUB_PW - if set, push to Dockerhub
 # AWS_PROFILE_NAME - if set, will use profile to push to ECR
 
-# Usage ./scripts/update_smilecdr.sh /path/to/docker-img-tarball [image tag]
+# Usage ./scripts/update_smilecdr.sh /path/to/docker-img-tarball [aws cli version] [image tag]
 
 set -eo pipefail
 
-echo "✔ Begin updating Smile CDR image ..."
+echo "⏳ Begin updating Smile CDR image ..."
 
 if [[ -z $1 ]];
 then
@@ -20,16 +20,25 @@ then
     exit 1
 fi
 
+# Configure AWS CLI version 
+# AWS CLI version 1 will soon be deprecated, so when not provided, it defaults to 2
+AWS_CLIENT_VERSION=${2:-"2"}
+if [[ $AWS_CLIENT_VERSION != "1" ]] && [[ $AWS_CLIENT_VERSION != "2" ]];
+then
+    echo "❌ You must provide a valid AWS CLI version (e.g. 1 or 2)"
+    exit 1
+fi
+
 # Use supplied image tag or make one from the tarball
 DOCKER_TARBALL_PATH=$1
 DOCKER_TARBALL=$(basename $1)
-DOCKER_REPO="232196027141.dkr.ecr.us-east-1.amazonaws.com/kf-smile-cdr"
-if [[ -z $2 ]];
+DOCKER_REPO="232196027141.dkr.ecr.us-east-1.amazonaws.com/kf-strides-smile-cdr"
+if [[ -z $3 ]];
 then
     DOCKER_IMAGE_TAG=${DOCKER_TARBALL#"smilecdr-"}
     DOCKER_IMAGE_TAG=${DOCKER_IMAGE_TAG%"-docker.tar.gz"}
 else
-    DOCKER_IMAGE_TAG=$2
+    DOCKER_IMAGE_TAG=$3
 fi
 DOCKER_IMAGE="$DOCKER_REPO:$DOCKER_IMAGE_TAG"
 
@@ -42,9 +51,19 @@ docker tag smilecdr:latest $DOCKER_IMAGE
 if [[ -n $AWS_PROFILE_NAME ]];
 then
     # Use profile if supplied
-    passwd=$(aws --profile="$AWS_PROFILE_NAME" ecr get-login --region us-east-1 | awk '{ print $6 }')
+    if [[ $AWS_CLIENT_VERSION == "1" ]];
+    then
+        passwd=$(aws --profile="$AWS_PROFILE_NAME" ecr get-login --region us-east-1 | awk '{ print $6 }')
+    else
+        passwd=$(aws --profile="$AWS_PROFILE_NAME" --region us-east-1 ecr get-login-password)
+    fi
 else
-    passwd=$(aws ecr get-login --region us-east-1 | awk '{ print $6 }')
+    if [[ $AWS_CLIENT_VERSION == "1" ]];
+    then
+        passwd=$(aws ecr get-login --region us-east-1 | awk '{ print $6 }')
+    else
+        passwd=$(aws --region us-east-1 ecr get-login-password)
+    fi
 fi
 echo "Pushing $DOCKER_IMAGE ..."
 docker login -u AWS -p $passwd "$DOCKER_REPO"
