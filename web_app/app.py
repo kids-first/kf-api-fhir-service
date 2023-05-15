a3d4af23f090102d5205be63121956f3ded01ce5
Natasha Singh <dukedesi22@gmail.com>
2023-05-15 17:49:38 -0400

    :sparkles: Create real keycloak proxy endpoint for testing

--- web_app/app.py
+++ web_app/app.py
@@ -11,21 +11,36 @@ import auth
 app = Flask(__name__)
 
 
-@app.post('/keycloak-proxy/<keycloak_endpoint>')
-def auth_sandbox(keycloak_endpoint):
+@app.post('/token')
+def auth_sandbox():
     """
     Test auth stuff out with keycloak
     """
-    if keycloak_endpoint == "token":
-        kwargs = request.get_json()
-        if request.args.get("issuer"):
-            kwargs.update({
-                "issuer": request.args.get("issuer")
-            })
-        token = auth.get_access_token(**kwargs)
-        return jsonify(token)
-    else:
-        abort(404)
+    kwargs = request.get_json()
+    if request.args.get("issuer"):
+        kwargs.update({
+            "issuer": request.args.get("issuer")
+        })
+    token = auth.get_access_token(**kwargs)
+
+    return jsonify(token)
+
+
+@app.post('/keycloak-proxy')
+def keycloak():
+    """
+    Forward requests to keycloak whether it is internal inside docker network
+    or in an externally resolvable network
+    """
+    payload = request.get_json()
+
+    http_op = payload.pop("http_operation", "get")
+    endpoint = payload.pop("endpoint", None)
+    kwargs = payload.get("kwargs", None)
+
+    resp = auth.send_request(http_op, endpoint, **kwargs)
+
+    return jsonify(resp.json())
 
 
 if __name__ == "__main__":
